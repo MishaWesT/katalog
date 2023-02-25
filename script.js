39c7659ff7227c8d6c3f9008036279e3e1d49b87
MishaWesT <115925118+MishaWesT@users.noreply.github.com>
2023-02-25 19:27:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ btnRight.addEventListener("click", function () {
         slides[i].classList.add("block");
     }
 })
+btnLeft.addEventListener("click", function () {
+    ++i
+    if (i >= slides.length) {
+        slides[i+1].classList.remove("block");
+        i = 0;
+        slides[i].classList.add("block");
+    } else {
+        slides[i+1].classList.remove("block");
+        slides[i].classList.add("block");
+    }
+})
